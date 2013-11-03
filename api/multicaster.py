519b6bc70d89659f58cba9905e6a2ae9eac6e3d1
Sadish Ravi <sadish.r@gmail.com>
2013-11-03 12:29:26 -0500

    cleanup and pydocs

--- api/multicaster.py
+++ api/multicaster.py
@@ -6,30 +6,48 @@ from api.multicastservice import get_routing, get_recipients_assigned_for_routin
 
 @app.route('/')
 def hello():
-    return 'Welcome to SendHub Message Routing API Home. Please POST to /api/sendhub/v1.0/routes for routing request.'
+    """
+    Home URL welcome request handler
+    """
+    msg = 'Welcome to SendHub Message Routing API Home. Please POST to /api/sendhub/v1.0/routes for routing request.'
+    return jsonify({"message": msg})
 
 
 @app.errorhandler(404)
 def not_found(error):
+    """
+    404 Error handler
+    """
     return make_response(jsonify({'error': 'Not found'}), 404)
 
 
 @app.errorhandler(400)
 def not_found(error):
+    """
+    400 Error handler
+    """
     return make_response(jsonify({'error': 'Bad Request'}), 400)
 
 
 @app.errorhandler(500)
 def not_found(error):
+    """
+    500 Error handler
+    """
     return make_response(jsonify({'error': 'Internal Server Error'}), 500)
 
 
 @app.route('/api/sendhub/v1.0/routes', methods=['POST'])
 def multi_cast():
+    # parse the post json body
     data = request.json
+    # verify for post body presence and structure verification using jsonschema
     if not data or not is_valid(data, sendhub_schema):
         abort(400)
     recipients = data['recipients']
+    # generate routing table
     routing_table = get_routing(recipients)
+    # generate actual routing details based on above routing table and recipients
     recipients_routing = get_recipients_assigned_for_routing(routing_table, recipients)
+    # return the above routes as json and 200 ok
     return jsonify({"routes": recipients_routing})
--- api/multicastservice.py
+++ api/multicastservice.py
@@ -1,20 +1,35 @@
 __author__ = 'sravi'
 
+# Relays dict as represented in the problem description
 relays_dict = {
     "S": {"name": "Small", "throughput": 1, "cost": 0.01, "subnet": "10.0.1.0/24"},
     "M": {"name": "Medium", "throughput": 5, "cost": 0.05, "subnet": "10.0.2.0/24"},
     "L": {"name": "Large", "throughput": 10, "cost": 0.10, "subnet": "10.0.3.0/24"},
     "X": {"name": "Super", "throughput": 25, "cost": 0.25, "subnet": "10.0.4.0/24"}
 }
-#having this in desc sorted form instead of sorting it by hand
+
+# having this in desc sorted form instead of sorting it by hand
 relay_types = ["X", "L", "M", "S"]
 
 
 def get_host_ip_of_type(relay_type, instance):
+    """
+    Return a host ip address for a given relay type and instance value
+    @param: relay_type: String
+    @param: instance: int
+    @return: host_ip: String
+    returns 10.0.1.2 when relay_type is "S" and instance=2
+    """
     return relays_dict[relay_type]["subnet"].rpartition('.')[0] + "." + str(instance)
 
 
 def get_routing(recipients):
+    """
+    Build the routing meta info to determine the number of hosts needed per subnet category
+    for the given recipients
+    @param: recipients: List of recipient phone numbers
+    @return: rt: Routing info table of the form {"X":1, "L":0, "M":1, "S":1}
+    """
     recipients_count = len(recipients)
     rt = {}
     for i in range(len(relay_types)):
@@ -27,6 +42,12 @@ def get_routing(recipients):
 
 
 def get_recipients_assigned_for_routing(routing_table, recipients):
+    """
+    Build the actual routes using the routing table generated before and the recipients list
+    @param routing_table: Python dict of form {"X":1, "L":0, "M":1, "S":1}
+    @param recipients: Python List  ["123-456-5678", "345-245-2552", ...]
+    @return routes: Nested dictionary of routes with hosts and recipients assigned to each hosts
+    """
     routes = []
     start = 0
     for i in range(len(relay_types)):
