7401b2d9dff82802605befccad291958de1fce8e
Sadish Ravi <sadish.r@gmail.com>
2013-11-03 08:50:19 -0500

    testing travis

--- tests/function_test_multicaster.py
+++ tests/function_test_multicaster.py
@@ -14,8 +14,8 @@ class MultiCastServiceTest(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def test_api_valid_single_recipient(self):
-        data = {"message": "SendHub Rocks", "recipients": ["201-706-1186"]}
+    def test_api_valid_single_recipient_1(self):
+        data = {"message": "SendHub Rocks", "recipients": ["201-706-1185"]}
         response = self.app.post(path='/api/sendhub/v1.0/routes',
                                  data=json.dumps(data), content_type="application/json")
         resp_data = json.loads(response.data)
@@ -24,4 +24,14 @@ class MultiCastServiceTest(unittest.TestCase):
         assert len(resp_data['routes']) == 1
         assert resp_data['routes'][0]['ip'] == "10.0.1.1"
         assert len(resp_data['routes'][0]['recipients']) == 1
-        assert resp_data['routes'][0]['recipients'][0] == "201-706-1186"
+        assert resp_data['routes'][0]['recipients'][0] == "201-706-1185"
+
+    def test_api_valid_multi_recipient(self):
+        data = {"message": "SendHub Rocks", "recipients": ["201-706-1185", "551-208-6869"]}
+        response = self.app.post(path='/api/sendhub/v1.0/routes',
+                                 data=json.dumps(data), content_type="application/json")
+        resp_data = json.loads(response.data)
+        assert response.status_code == 200
+        assert resp_data is not None
+        assert len(resp_data['routes']) == 2
+
